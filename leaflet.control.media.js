bbf84249d60d86d8ea237b91bf980c0b8a6a2644
sirenhound <ethan@antioch.org.au>
2018-04-24 11:26:59 +0800

    refactoring and pruning

--- leaflet.control.media.js
+++ leaflet.control.media.js
@@ -22,7 +22,7 @@ L.Control.Media = L.Control.extend({
         console.log(pointer, this._start, this._end, this._layer, from, to);
       }
     }],
-
+    // Pausing continues to make animation frame requests, stopping stops making requests altogether
     stop: true, // stops, not changes speed of animation (0x is pause)
     record: false, // This seems necessary but will have its own set of problems requiring listeners all over the place
     stepForward: false, //  >|
@@ -34,7 +34,7 @@ L.Control.Media = L.Control.extend({
     pause: true,
     play: true,
     ffwd: true,
-    // Example symbology <<   <  <|   ||   |>  >    >>
+    // Example symbology ⏪ < <| ⏸ |>  ▶️ ⏩
     speeds: [-8, -4, -2, -1, -0.5, 0, 0.5, 1, 2, 4, 8] // Multiplier: An array of speeds to make available to this player
   },
   onAdd: function(map){
@@ -54,6 +54,9 @@ L.Control.Media = L.Control.extend({
     this._container = this._createControl();
     return this._container;
   },
+  /** Creates the DOM Element container to be added to the map
+  * @returns {HTMLDivElement} Element to add to one of the maps control corners
+  */
   _createControl: function(){
     var control = L.DomUtil.create('div');
     control.style.fontFamily = "initial"; // Undo leaflet map font that has inconsistant media control characters
@@ -79,32 +82,44 @@ L.Control.Media = L.Control.extend({
 
     return control;
   },
-  // TODO Customize L.Util.requestAnimFrame to have layer of control
+  /** Animation wrapper to feed timer data to animations so they can be played back at precision
+  * @param {Function}
+  * @param {object}
+  * @param {boolean}
+  * @todo Customize L.Util.requestAnimFrame to have layer of control
+  * @ignore
+  */
   requestAnimFrame:function(func, context, immed){
     var win = window;
-    var reqAnFrm = win.requestAnimationFrame || webk("RequestAnimationFrame");// ||i;
-    return immed && reqAnFrm === i ? void func.call(context) : reqAnFrm.call(win, L.Util.bind(func, context))
+    var reqAnFrm = win.requestAnimationFrame || webk("RequestAnimationFrame");
+    return immed && reqAnFrm === void 0 ? void func.call(context) : reqAnFrm.call(win, L.Util.bind(func, context))
 
     //return L.Util.requestAnimFrame.apply(null, arguments);
   },
-
+  /** Runs the control clock at the given speed
+  * @param {number} [speed = 1] - The multiplier to run the animation
+  */
   play: function(speed){
-    // Take out of this function.
-    /* refactor to general case later, for now input animation at 0ms and ends at 60000ms */
-
+    if (this.options.animationStart && this.options.duration && this.options.animationEnd){
+      console.warn("Options object has 'animationStart', 'duration' and 'animationEnd'. 'animationEnd' will be ignored");
+    }
+    // TODO check for errors if options not specified
     // where to start the specified animation (each control instance will have a time range)
-    this._animStart = this.options.animationStart;
+    this._animStart = this.options.animationStart || this.options.animationEnd - this.options.duration;
 
-    // does not seem necessary, however, perhaps duration is unknown but end time is.
-    this._animEnd = this._animStart + this.options.duration;
+    this._animEnd = this.options.duration? this._animStart + this.options.duration : this.options.animationEnd;
 
     // Handle pointer and multiplier if input differs from whats already playing
-    if (this._multiplier !== (speed === void 0? 1 : speed)){
-      this._multiplier = speed === void 0? 1 : speed;
+    // explicitly check for undefined or a 'falsy' speed of 0 will become 1
+    speed = (speed === void 0? 1 : speed);
+    if (this._multiplier !== speed){
+      this._multiplier = speed;
       this._timestampWhenSpeedChanged = performance.now();
+      // points to current time on control clock
       this._pointer = this._pointer || this._animStart;
       this._pointerWhenSpeedChanged = this._pointer;
     }
+
     if (this._stopped){ // Do not add additional requests
       this._stopped = false; //TODO unset all others too (except _recording, since we'd like to that at different speeds)
       this._playing = true;
@@ -113,7 +128,8 @@ L.Control.Media = L.Control.extend({
       requestAnimationFrame(this.duringPlayback.bind(this));
     }
   },
-
+  /** Stops the playback of control clock and prevents further animation frames
+  */
   stop: function(){
     this._playing = false;
     this._stopped = true;
@@ -124,28 +140,31 @@ L.Control.Media = L.Control.extend({
   * @param {AnimFunction} func - The function that represents the animation. It should accept a timestamp argument
   currently also gets a status object, but will be fixed to be given other arguments too
   * @param {Object} cxt - The 'this' context to call the function with.
-  * @param {DOMHighResTimeStamp} timestamp - Maybe it's a highres timestamp. check.
+  * @param {DOMHighResTimeStamp} timestamp - Maybe it's a highres timestamp? TODO check.
   * @param {...Object} rest - Additional arguments for AnimFunction.
   */
   runAnimation: function(func, cxt, timestamp, ...rest){
-
-    var dateObj = new Date(timestamp);//don't do this in final draft, just write a method to do it yourself without instances
-
+    // Show current pointer of control clock
+    var dateObj = new Date(timestamp);//don't do this in final draft, just write a method to do it yourself without instances (memory leak)
     console.log("pointer: "+dateObj.toUTCString());
 
     rest.unshift(timestamp); // push timestamp to front of rest array, so the animation function can be called with its arguments
     func.apply(cxt, rest);
   },
+  /** Has been moved to options property
+  * @ignore
+  */
   _animFunc: function(pointer, funcArgs){
+    //
     var from = 0; var to = 1;
     this._start = this._start || pointer; // arbitrarily start this sample animation at the first pointer value
     this._end = this._start + 30000; // just say it lasts 30sec
     // trying to incorporate follow layer
     //this._followHandler.followLayer(editableGroup.getLayers()[0], )
     this._layer = this._layer || editableGroup.getLayers()[0];
-    this._m_lineTo(pointer, this._start, this._end, this._layer, from, to);
   },
-  /** Requests frames while a control is running
+  /** Animation Frame Request Callback Function. Requests frames while a control is running
+  * @param {DOMHighResTimeStamp} timestamp
   */
   duringPlayback: function(timestamp){
     // number relative to anim NOT timestamp 0...9 mx1: 0123456789
@@ -199,6 +218,7 @@ L.Control.Media = L.Control.extend({
       this._playing = false; // prevent active animations when added to map.
     }
   },
+
   addAnimation: function(func, cxt, ...rest){
     var animationObject = {
       context: cxt,
@@ -208,9 +228,3 @@ L.Control.Media = L.Control.extend({
     this.options.animations.push(animationObject);
   }
 });
-/*
-  speedMultiplier = 2;
-  animDuration = 10000;
-  p = performance.now(); // needs to be set outside the step function or it will keep increasing with the timestamp
-  forward(performance.now());
-*/
